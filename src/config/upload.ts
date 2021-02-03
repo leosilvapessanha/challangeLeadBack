821e58a232d67456802c7376f89a4f7cf895e9fd
Léo <leonardosilvapessanha@gmail.com>
2021-02-03 20:00:25 -0300

    debug

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -6,7 +6,7 @@ export default {
   storage: multer.diskStorage({
     destination: path.resolve(__dirname, '..', '..', 'tmp'),
     filename(request, file, callback) {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
 
       return callback(null, fileName);
